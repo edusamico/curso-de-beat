c8e6149cfe496bf728de0349acef6a903d4cc845
edusamico <edusamico@gmail.com>
2021-12-15 18:10:56 -0300

    adicionado links externos e iniciado o js do storie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 /**STORIE KIT**/
-
+function viewstorie1() {
+        if (document.querySelector('#slideDAW').classList.contains('displayBlock')) {
+        document.querySelector('#slideDAW').classList.remove('displayBlock');
+        document.querySelector('#slideDAW').classList.add('displayNone');
+    } else {
+        document.querySelector('#slideDAW').classList.remove('displayNone');
+        document.querySelector('#slideDAW').classList.add('displayBlock');   
+    }
+}
 /**DRUM KIT**/
 document.body.addEventListener('keyup', (event)=> {
     playSound(event.code.toLowerCase() );
